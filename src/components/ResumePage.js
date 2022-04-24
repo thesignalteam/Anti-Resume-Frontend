1c630d4ac31d788fb553514aa1af5a37e708707f
marcel kida <47465692+marcelkida@users.noreply.github.com>
2022-04-24 18:21:25 -0400

    front end changes
    Frontend - edit AllResumes.js to include 2022 seniors - Marcel
    Frontend - edit Landing.js to include 2022 senior carousel - Marcel
    Also added 2022 to navbar

--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -11,6 +11,7 @@ class ResumePage extends Component {
   constructor(props, { match }) {
     super();
     this.state = {
+      resumes_2022: [],
       resumes_2021: [],
       resumes_2020: [],
       resumes_2019: [],
@@ -26,6 +27,21 @@ class ResumePage extends Component {
   }
 
   componentDidMount = () => {
+    fetch(`${ip}/api/getAllResumes/senior/2021`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          // console.log(result);
+          this.setState({
+            resumes_2022: result
+          });
+        },
+
+        (error) => {
+          console.log("error is " + error);
+        }
+      )
+
     fetch(`${ip}/api/getAllResumes/senior/2021`)
       .then(res => res.json())
       .then(
@@ -119,7 +135,7 @@ class ResumePage extends Component {
   }
 
   getCurrentResume = (resumes) => {
-    resumes = this.state.resumes_2021.concat(this.state.resumes_2020.concat(this.state.resumes_2019).concat(this.state.resumes_alums).concat(this.state.resumes_faculty).concat(this.state.resumes_curf))
+    resumes = this.state.resumes_2022.concat(this.state.resumes_2021).concat(this.state.resumes_2020.concat(this.state.resumes_2019).concat(this.state.resumes_alums).concat(this.state.resumes_faculty).concat(this.state.resumes_curf))
     let resumeId = this.state.resumeId
     let resumeClass = this.state.resumeClass
     let gridValues = []
